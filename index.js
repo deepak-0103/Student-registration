e8b0eb97ba4ef6aeebd03f0aa28e9a823688e603
deepak-0103 <deepakkumar.kumar403@gmail.com>
2025-12-25 23:31:07 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,71 @@
+var selectedRow = null;
+
+function onFormSubmit() {
+    var formData = readFormData();
+    if (selectedRow == null)
+        insertNewRecord(formData);
+    else
+        updateRecord(formData);
+    resetForm();
+
+}
+    
+
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["clas"] = document.getElementById("clas").value;
+    formData["contact"] = document.getElementById("contact").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.clas;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.contact;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = `<a onclick="editRecord(this)"> Edit</a>
+                       <a onclick="deleteRecord(this)"> Delete</a>`; 
+}
+
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("clas").value = "";
+    document.getElementById("contact").value = "";
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("clas").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("contact").value = selectedRow.cells[3].innerHTML;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("studentList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}       
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault();
+    onFormSubmit();
+});     
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.clas;
+    selectedRow.cells[3].innerHTML = formData.contact;
+}
